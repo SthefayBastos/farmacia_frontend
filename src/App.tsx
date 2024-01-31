16cb6cf7e1a58f4b285570b42ef7234a78e3dd5d
Sthefany Sousa <sthefanyssbastos@hotmail.com>
2024-01-31 11:31:08 -0300

    rotas definidas-conclusão projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './components/footer/Footer'
-
-import Home from './components/home/Home'
-import Navbar from './components/navbar/Navbar'
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
+import DeletarCategoria from "./components/categoria/deletarCategoria/DeletarCategoria"
+import FormCategoria from "./components/categoria/formCategoria/FormCategoria"
+import ListaCategorias from "./components/categoria/listarcategorias/ListarCategorias"
+import Home from "./components/home/Home"
 
 
 function App() {
   return (
     <>
-    
-    <BrowserRouter>
+      
+        <BrowserRouter>
           <Navbar />
-          
-          <div className='min-h-[80vh]'>
+          <div className="min-h-[80vh]">
             <Routes>
-              <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
-              </Routes>
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+              <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+              <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+            </Routes>
           </div>
-
           <Footer />
         </BrowserRouter>
+      
     </>
   )
 }
-   
+
 export default App
--- src/components/categoria/cardCategoria/CardCategoria.tsx
+++ src/components/categoria/cardCategoria/CardCategoria.tsx
@@ -9,12 +9,12 @@ interface CardCategoriasProps{
 function CardCategoria({ categoria }: CardCategoriasProps) {
     return (
         <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-            <header className='py-2 px-6 bg-slate-400 font-bold text-2xl'>Categoria</header>
-            <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.tipo}</p>
+            <header className='py-2 px-6 bg-teal-900 font-bold text-2xl'>Categoria</header>
+            <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.nome}</p>
             
             <div className="flex">
                 <Link to={`/editarcategoria/${categoria.id}`}
-                    className='w-full font-bold bg-slate-400 hover:bg-slate-700
+                    className=' font-bold w-full bg-teal-900  text-gray-400 hover:bg-slate-700
                         flex items-center justify-center py-2'>
                     <button>Editar</button>
                 </Link>
